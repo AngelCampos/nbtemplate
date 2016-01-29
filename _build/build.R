06e5db5b5ad354a0c869d0aed27fb49603820ec6
Phil Ross <philippross369@gmail.com>
2016-01-28 23:56:12 -0500

    updated build script

--- _build/build.R
+++ _build/build.R
@@ -1,28 +1,22 @@
 library(knitr)
 
 build <- function(a) {
-  
-  # fall back on '/' if baseurl is not specified
-  baseurl <- servr:::jekyll_config('.', 'baseurl', '/')
-  knitr::opts_knit$set(base.url = baseurl)
-  
+
   # fall back on 'kramdown' if markdown engine is not specified
   markdown <- servr:::jekyll_config('.', 'markdown', 'kramdown')
-  
+
   # see if we need to use the Jekyll render in knitr
   if (markdown == 'kramdown') {
     knitr::render_jekyll()
   } else knitr::render_markdown()
-  
+
   d <- gsub('^_|[.][a-zA-Z]+$', '', a[1])
   ## Default to png since svgs with lots of points can be huge and also choke pandoc
   ## Cache in an underscored dir since we never want to commit cache
-  ## figures in a usable path, though excluded in _config.yml since we embed as data_uris
   knitr::opts_chunk$set(
-    root.dir   = "../..",
     fig.path   = sprintf('img/%s/', d),
     cache.path = sprintf('_cache/%s/', d),
-    cache      = TRUE,
+    cache      = FALSE,
     comment    = NA,
     message    = FALSE,
     warning    = FALSE,
@@ -30,11 +24,18 @@ build <- function(a) {
     fig.cap    = "",
     fig.show   = "hold",
     fig.width  = 10,
-    fig.height = 8
+    fig.height = 8,
+    fig.align  = "center"
   )
-  
-  knitr::opts_knit$set(base.url = paste0(baseurl, "/"), verbose = TRUE, progress = TRUE)
-  knitr::opts_knit$set(width = 70)
+
+  knitr::opts_knit$set(
+    root.dir   = "./",
+    base.url   = "/",
+    verbose    = TRUE,
+    progress   = TRUE,
+    width      = 80
+  )
+
   knitr::knit(a[1], a[2], quiet = FALSE, encoding = 'UTF-8', envir = .GlobalEnv)
 }
 
@@ -43,3 +44,4 @@ local({
   a <- commandArgs(TRUE)
   build(a)
 })
+
